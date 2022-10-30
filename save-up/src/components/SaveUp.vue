fe7dbd2a5fed1bd0429618702eb1214e46da2c95
redhamo <1043172934@qq.com>
2022-10-30 15:08:42 +0800

    import element-plus

---
+++ save-up/src/components/SaveUp.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  function log(info:String) {
+    console.log(info)
+  }
+</script>
+
+<template>
+  <div class="save-up">
+    <el-button type="primary" size="default" @click="log('123')">1211113</el-button>
+  </div>
+</template>
+
+<style scoped>
+.save-up{
+  flex: 1;
+}
+</style>
